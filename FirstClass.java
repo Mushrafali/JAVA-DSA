56027d3646921eb2b554f43f971fc5e7f0560dbb
Mushrafali Momin <113009766+Mushrafali@users.noreply.github.com>
2024-07-05 21:52:00 +0530

    Variables

---
+++ FirstClass.java
@@ -0,0 +1,42 @@
+/**
+ * main
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        //variables
+        String name = "tony stark";
+        int age = 52;
+        double price = 25.25;
+        int a = 12;
+        int b = 46;
+
+        name = "iron man";
+        age = 64;
+
+        int sum = a + b;
+
+        System.out.println(sum);
+        
+        System.out.println(name);
+        
+        System.out.println(age);
+
+
+        System.out.print("HEllo");   /* output will be in the same line if we dont use ln or \n */
+        System.out.println("HEllo");
+        System.out.println("Hello World");
+        System.out.println("*");
+        System.out.print("**\n");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.print("   *\n  **\n ***\n****");
+        System.out.print("***");
+        System.out.print("****");
+
+        
+
+
+    }
+}
